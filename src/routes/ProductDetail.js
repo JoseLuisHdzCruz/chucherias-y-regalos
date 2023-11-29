4a742640a017dc38169a676973ef15bad3dd52ce
Jose Luis Hernandez De La Cruz <20211032@UTHH.EDU.MX>
2023-11-28 22:35:00 -0600

    ùpdate files

--- src/routes/ProductDetail.js
+++ src/routes/ProductDetail.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import '../styles/styles.css';
+import React, { useState } from "react";
+import "../styles/styles.css";
 
 const ProductDetail = () => {
   const [quantity, setQuantity] = useState(1);
@@ -18,7 +18,9 @@ const ProductDetail = () => {
     <main>
       <h5 className="fw-semibold">Inicio > Detalle producto</h5>
 
-      <h3 className="title-pag fw-bold text-uppercase mt-3">Detalle del producto</h3>
+      <h3 className="title-pag fw-bold text-uppercase mt-3">
+        Detalle del producto
+      </h3>
       <hr className="hr-primary" />
 
       <div className="detail-product">
@@ -26,35 +28,39 @@ const ProductDetail = () => {
           <div className="card mb-3">
             <div className="row g-0">
               <div className="col-md-6">
-                <img src="./images/R (1).jpg" className="img-fluid rounded-start mt-4" alt="Caja Registradora de juguete" />
+                <img
+                  src="./oso de peluche.jpg"
+                  className="img-fluid rounded-start mt-4"
+                  alt="Oso de peluche"
+                />
               </div>
               <div className="col-md-6">
                 <div className="card-body">
-                  <h5 className="card-title">Caja registradora de juguete para niñas</h5>
+                  <h5 className="card-title">Oso Osito De Peluche Teo</h5>
                   <h2 className="text-price mt-4 fw-bold">$ 150.00</h2>
                   <p className="card-text">
-                    Estimado cliente. Hola. El juego de juguetes para niños de la
-                    caja registradora es adecuado para niñas y niños y es el mejor
-                    regalo para los niños.Te regalamos 3 pilas , puede usar el
-                    producto directamente. Tenemos colores rosa y gris para
-                    elegir. El producto coincide con la imagen. Esperando tu
-                    pedido. Estamos aquí para servirle.
+                    Hermoso osito Teo súper esponjosito y suave. Calidad
+                    PREMIUM. <br /> Ideal para arreglos de mesa y regalo. ****Si no
+                    encuentra los sets de este mismo modelo puede preguntar por
+                    ellos.**** <br /> <br /> Tamaño: 25cm (en diagonal de oreja a pie) <br /> Color:
+                    Café **COMO SE VE EN LAS FOTOS** <br /> Material: Poliéster <br /> Edad
+                    recomendada: +3 años
                   </p>
-
                 </div>
               </div>
             </div>
           </div>
         </div>
         <div className="colum-add">
-
           <div className="card mb-4">
-              <div className="card-title text-center">
-                Escanea con la app!!
-              </div>
+            <div className="card-title text-center">Escanea con la app!!</div>
             <div className="card-body">
               <div className="qr">
-                <img src="./images/qrcode-generado.png" className="img-fluid rounded-start mt-4" alt="..." />
+                <img
+                  src="./images/qrcode-generado.png"
+                  className="img-fluid rounded-start mt-4"
+                  alt="..."
+                />
               </div>
             </div>
           </div>
@@ -67,9 +73,13 @@ const ProductDetail = () => {
               <div className="cant mt-4">
                 <h4>Cantidad</h4>
                 <div className="counter">
-                  <button className="decrement" onClick={handleDecrement}>-</button>
+                  <button className="decrement" onClick={handleDecrement}>
+                    -
+                  </button>
                   <span className="value">{quantity}</span>
-                  <button className="increment" onClick={handleIncrement}>+</button>
+                  <button className="increment" onClick={handleIncrement}>
+                    +
+                  </button>
                 </div>
               </div>
               <div className="cont-buttons text-center mt-4">
@@ -78,9 +88,7 @@ const ProductDetail = () => {
               </div>
             </div>
           </div>
-          
         </div>
-        
       </div>
     </main>
   );
