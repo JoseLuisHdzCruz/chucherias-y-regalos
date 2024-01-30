dd89f96cabe7ed071dbcc6e94cfeefda009d8e60
Itzel Doroteo Martinez <20211311@uthh.edu.mx>
2024-01-30 15:37:56 -0600

    update politicas de seguridad de contenido

--- src/components/PageTitle.js
+++ src/components/PageTitle.js
@@ -8,17 +8,15 @@ const PageTitle = ({ title }) => (
     <meta
       http-equiv="Content-Security-Policy"
       content="
-      default-src 'self';
-      script-src 'self' 'unsafe-inline' https://cdn.userway.org code.tidio.co;
-      style-src 'self' 'unsafe-inline';
-      connect-src 'self' https://api.userway.org https://api.tidio.co;
-      font-src 'self' data:;
-      img-src 'self' data: https://cdn.userway.org code.tidio.co;
-      frame-src https://cdn.userway.org code.tidio.co;
-      connect-src 'self' http://localhost:5000;
-    "
+        default-src 'self';
+        script-src 'self' 'unsafe-inline' https://cdn.userway.org code.tidio.co;
+        style-src 'self' 'unsafe-inline';
+        connect-src 'self' https://api.userway.org https://api.tidio.co http://localhost:5000;
+        font-src 'self' data:;
+        img-src 'self' data: https://cdn.userway.org code.tidio.co;
+        frame-src https://cdn.userway.org code.tidio.co;
+      "
     />
-
     <title>{title}</title>
   </Helmet>
 );
