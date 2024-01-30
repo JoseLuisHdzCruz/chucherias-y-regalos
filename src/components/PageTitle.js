8c15db9a237cdc4d4eb84f9b315a7848ceee3cde
Itzel Doroteo Martinez <20211311@uthh.edu.mx>
2024-01-30 15:23:26 -0600

    Politica

--- src/components/PageTitle.js
+++ src/components/PageTitle.js
@@ -1,10 +1,24 @@
 // PageTitle.js
-import React from 'react';
-import { Helmet } from 'react-helmet';
+import React from "react";
+import { Helmet } from "react-helmet";
 
 const PageTitle = ({ title }) => (
   <Helmet>
     {/* <meta http-equiv="Content-Security-Policy" content="default-src 'self'" /> */}
+    <meta
+      http-equiv="Content-Security-Policy"
+      content="
+      default-src 'self';
+      script-src 'self' 'unsafe-inline' https://cdn.userway.org code.tidio.co;
+      style-src 'self' 'unsafe-inline';
+      connect-src 'self' https://api.userway.org https://api.tidio.co;
+      font-src 'self' data:;
+      img-src 'self' data: https://cdn.userway.org code.tidio.co;
+      frame-src https://cdn.userway.org code.tidio.co;
+      connect-src 'self' http://localhost:5000;
+    "
+    />
+
     <title>{title}</title>
   </Helmet>
 );
