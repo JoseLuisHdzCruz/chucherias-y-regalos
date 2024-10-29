981cf3b7dc82e9360ffcf67652c1d509f3890c09
Jose Luis Hernandez De La Cruz <dareckluis@gmail.com>
2024-10-29 09:34:00 -0600

    actualizar el service worker

--- workbox-config.js
+++ workbox-config.js
@@ -1,27 +1,41 @@
 module.exports = {
-    globDirectory: "build/",
-    globPatterns: [
-      "**/*.{html,js,css,png,jpg,svg}"
-    ],
-    swDest: "build/service-worker.js",
-    runtimeCaching: [
-      {
-        urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
-        handler: 'CacheFirst',
-        options: {
-          cacheName: 'images-cache',
-          expiration: {
-            maxEntries: 50, 
-          },
+  globDirectory: "build/",
+  globPatterns: [
+    "**/*.{html,js,css,png,jpg,svg}"
+  ],
+  swDest: "build/service-worker.js",
+  runtimeCaching: [
+    {
+      urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
+      handler: 'CacheFirst',
+      options: {
+        cacheName: 'images-cache',
+        expiration: {
+          maxEntries: 50,
+          maxAgeSeconds: 30 * 24 * 60 * 60, // 30 días
         },
       },
-      {
-        urlPattern: /\.(?:js|css)$/,
-        handler: 'StaleWhileRevalidate',
-        options: {
-          cacheName: 'static-resources',
+    },
+    {
+      urlPattern: /\.(?:js|css)$/,
+      handler: 'StaleWhileRevalidate',
+      options: {
+        cacheName: 'static-resources',
+        expiration: {
+          maxAgeSeconds: 7 * 24 * 60 * 60, // 7 días
         },
       },
-    ],
-  };
-  
+    },
+    {
+      // Maneja archivos HTML con NetworkFirst
+      urlPattern: /.*\.html$/,
+      handler: 'NetworkFirst',
+      options: {
+        cacheName: 'html-cache',
+        expiration: {
+          maxEntries: 20,
+        },
+      },
+    },
+  ],
+};
